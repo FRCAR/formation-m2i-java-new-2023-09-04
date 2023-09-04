df9af1e7340e503ec77f03af384e378f73d4b3e2
François <francois.caremoli@wanadoo.fr>
2023-09-04 14:33:23 +0200

    Ajout commentaires lambda

--- main-module/src/main/java/com/bigcorp/project/main/lambda/Window.java
+++ main-module/src/main/java/com/bigcorp/project/main/lambda/Window.java
@@ -3,48 +3,47 @@ package com.bigcorp.project.main.lambda;
 import java.util.function.Consumer;
 
 public class Window {
-	
-	
+
 	public static void main(String[] args) {
 		Window window = new Window();
-		
 
-		Button lambdaButton2 = (Integer tempsDuClick) -> {return "lambda button appuyé pendant " + tempsDuClick ;};
-		//équivaut à : 
-		Button lambdaButton = tempsDuClick -> "lambda button appuyé pendant " + tempsDuClick ;
-		
-		Consumer<Object> basicConsumer = (Object a) -> {System.out.println(a);};
-		//équivaut à : 
+		Button lambdaButton2 = (Integer tempsDuClick) -> {
+			return "lambda button appuyé pendant " + tempsDuClick;
+		};
+		// équivaut à :
+		Button lambdaButton = tempsDuClick -> "lambda button appuyé pendant " + tempsDuClick;
+
+		Consumer<Object> basicConsumer = (Object a) -> {
+			System.out.println(a);
+		};
+		// équivaut à :
 		Consumer<Object> basicConsumer2 = a -> System.out.println(a);
-		
-		int b = 4;
-		
-		Consumer<Object> basicConsumer3 = a -> {
-			int c = b + 4;	
-			System.out.println(c);};
-		
 
-		
+		// Variable effectivement final, utilisable dans une lambda
+		int b = 4;
+		Consumer<Object> basicConsumer3 = a -> {
+			int c = b + 4;
+			System.out.println(c);
+		};
+
 		window.addButton(lambdaButton);
-		
-		//plein de lignes de codes plus tard
-		
+
+		// plein de lignes de codes plus tard, exécutera la lambda
 		window.execute();
-		
-		
+
 	}
-	
+
 	private Button button;
-	
+
 	public void addButton(Button button) {
 		this.button = button;
 	}
-	
+
 	public void execute() {
 		System.out.println("La fenête est activée");
 		System.out.println(this.button.click(45));
 		System.out.println("La fenête se désactive");
-		
+
 	}
 
 }
